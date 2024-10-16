5c434894a8518b9cf63ada4ef582a0577f0ac998
Bingine <alex.10.03.2006@gmail.com>
2024-10-16 14:32:17 +0200

    Feat: Added the discount label

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -1,11 +1,14 @@
 import styles from "./Price.module.css";
 
-function Price({ product }) {
+function Price({ product, isDiscountShown = true }) {
     return (
-        <>
+        <div className={styles.price}>
         <span className={styles.primaryPrice}>${product.discountPrice != null ? product.discountPrice : product.price}</span>
-    <span className={styles.secondaryPrice}>{product.discountPrice != null ? `\$${product.price}` : ""}</span>
-        </>
+        <span className={styles.secondaryPrice}>{product.discountPrice != null ? `\$${product.price}` : ""}</span>
+        <span className={styles.discount} style={{
+                    display: `${(product.discountPrice != null && isDiscountShown) ? "block" : "none"}`,
+                }}>-{Math.floor(100 - (product.discountPrice * 100 / product.price))}%</span>
+        </div>
     );
 }
 
