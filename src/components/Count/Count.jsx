88b9e338bc6348dedb59eef7d29b6209d885f9f5
Bingine <alex.10.03.2006@gmail.com>
2024-10-16 16:55:48 +0200

    Feat: now the count component is functional && styled

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,15 +1,23 @@
 import styles from "./Count.module.css";
 
-
+import { useState } from "react";
 
 function Count({ product }) {
-    if (!product.count) product.count = 1;
+    const [count, setCount] = useState((product.count ? product.count : 1));
+
+    function decrement() {
+        if (count > 1) setCount(count - 1);
+    }
+
+    function increment() {
+        setCount(count + 1);
+    }
 
     return (
     <div className={styles.count}>
-        <button>-</button>
-        <span>{product.count}</span>
-        <button>+</button>
+        <button onClick={decrement}>-</button>
+        <span>{count}</span>
+        <button onClick={increment}>+</button>
     </div>
     );
 }
