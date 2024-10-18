000e44447446a6ba993c2444ba1c0dff6c6a785a
Bingine <alex.10.03.2006@gmail.com>
2024-10-18 16:14:27 +0200

    Fix: Added the cards to the footer (also, the footer is adaptive now)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,9 @@ import GoogleMapReact from "google-map-react";
 
 import styles from "./Footer.module.css";
 
+import Viber from "/src/assets/images/icons/socials/viber.svg";
+import Instagram from "/src/assets/images/icons/socials/instagram.svg";
+
 function Footer() {
   const defaultProps = {
     center: {
@@ -10,19 +13,40 @@ function Footer() {
     },
     zoom: 18,
   };
-  const AnyReactComponent = ({ text }) => (
+  const Map = ({ text }) => (
     <div style={{ height: "100px" }}>{text}</div>
   );
 
   return (
     <footer className={styles.footer}>
       <h3>Contact</h3>
+      <div className={styles.cards}>
+        <div>
+            <h5>Phone</h5>
+            <p>+49 30 915-88492</p>
+        </div>
+        <div>
+            <h5>Socials</h5>
+            <p>
+                <a href="https://instagram.com" target="_blank"><img src={Instagram} /></a>
+                <a href="https://viber.com" style={{marginLeft: "16px",}} target="_blank"><img src={Viber} /></a>
+            </p>
+        </div>
+        <div>
+            <h5>Address</h5>
+            <p>Wallstraẞe 9-13, 10179 Berlin, Deutschland</p>
+        </div>
+        <div>
+            <h5>Working Hours</h5>
+            <p>24 hours a day</p>
+        </div>
+      </div>
       <div className={styles.map}>
         <GoogleMapReact
           defaultCenter={defaultProps.center}
           defaultZoom={defaultProps.zoom}
         >
-          <AnyReactComponent
+          <Map
             lat={defaultProps.center.lat}
             lng={defaultProps.center.lng}
             text="ITCareerHub"
