51dc5f2b64e2bc47e0951996c43b731671af6f97
Bingine <alex.10.03.2006@gmail.com>
2024-10-22 10:21:37 +0200

    Feat: Now the shopping cart page has notification and order form

--- src/views/ShoppingCart/ShoppingCart.jsx
+++ src/views/ShoppingCart/ShoppingCart.jsx
@@ -2,21 +2,36 @@ import styles from "./ShoppingCart.module.css";
 
 import { useNavigate } from "react-router-dom";
 
+import { useState } from "react";
+
+import { send } from "/src/services/order.js";
+
+import useUserStore from "/src/stores/user.js";
 import useShoppingCartStore from "/src/stores/shoppingCart.js";
 
 import Button from "/src/components/Button/Button.jsx";
+import OrderForm from "/src/components/OrderForm/OrderForm.jsx";
 import SectionBar from "/src/components/SectionBar/SectionBar.jsx";
+import Notification from "/src/components/Notification/Notification.jsx";
 import ShoppingCartCard from "/src/components/ShoppingCartCard/ShoppingCartCard.jsx";
 
-function ShoppingCart({}) {
+function ShoppingCart() {
+    const user = useUserStore(state => state.user);
+
     const products = useShoppingCartStore(state => state.products);
+    const clear = useShoppingCartStore(state => state.clear);
 
     const navigate = useNavigate();
 
-    console.log(products);
+    const [notification, setNotification] = useState({
+        isShown: false,
+        title: "Congratulations!",
+        message: "Your order has been successfully placed on the website.\n\nA manager will contact you shortly to confirm your order.",
+    });
 
     return (
         <>
+        <Notification notification={notification} setNotification={setNotification} onClose={() => clear()} />
         <SectionBar style={{margin: "40px 40px"}} title="Shopping Cart" button={{ text: "Back to the store", route: "/products" }} />
         { Object.keys(products).length === 0 ? 
             <>
@@ -24,7 +39,18 @@ function ShoppingCart({}) {
                 <Button initialText="Continue Shopping" clickedText="Redirecting..." onClick={() => navigate("/products")} dependencies={{}} style={{width: "313px", marginTop: "32px", marginBottom: "80px"}} />
             </> : 
             <>
+            <section className={styles.shoppingCart}>
+                <div className={styles.cards}>
                 { Object.values(products).map((product) => <ShoppingCartCard key={product.id} product={product} />) }
+                </div>
+                <OrderForm onSubmit={async () => {
+                    const result = await send(user, products);
+
+                    if (result) {
+                        setNotification({ ...notification, isShown: true });
+                    }
+                }} />
+            </section>
             </>
         }
         </>
