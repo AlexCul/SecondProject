c6e414881406d0eeddc2f7d8419cac39986fb7c2
Bingine <alex.10.03.2006@gmail.com>
2024-10-22 09:23:51 +0200

    Fix: now all of the category cards are links to their category page

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -3,8 +3,10 @@ import styles from "./CategoryCard.module.css";
 function CategoryCard({ category }) {
     return (
         <div className={styles.card}>
+        <a href={`/categories/${category.id}`}>
             <img src={`http://localhost:3333/${category.image}`} alt="" />
             <span>{category.title}</span>
+        </a>
         </div>
     );
 }
