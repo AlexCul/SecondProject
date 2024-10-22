e60472f08e2b2f72cf6986c890d1a1e29f457af5
Bingine <alex.10.03.2006@gmail.com>
2024-10-22 15:42:45 +0200

    Fix: Fixed the loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,11 @@
 import styles from "./Loader.module.css";
 
 function Loader() {
-    return <div className="loader"></div>;
+    return (
+    <div className={styles.wrapper}>
+        <div className={styles.loader}></div>
+    </div>
+    );
 }
 
 export default Loader;
