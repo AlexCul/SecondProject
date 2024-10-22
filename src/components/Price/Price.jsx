5bd45baaee1018962c2bd248483a60ebc256cd99
Bingine <alex.10.03.2006@gmail.com>
2024-10-22 17:36:59 +0200

    Feat: Now we can specify font sizes for our price

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -1,12 +1,17 @@
 import styles from "./Price.module.css";
 
-function Price({ product, isDiscountShown = true }) {
+function Price({ product, isDiscountShown = true, fontSizes = {
+    primaryPrice: "40px",
+    secondaryPrice: "20px",
+    discount: "20px",
+} }) {
     return (
         <div className={styles.price}>
-        <span className={styles.primaryPrice}>${product.discountPrice != null ? product.discountPrice : product.price}</span>
-        <span className={styles.secondaryPrice}>{product.discountPrice != null ? `\$${product.price}` : ""}</span>
+        <span className={styles.primaryPrice} style={{fontSize: fontSizes.primaryPrice}}>${product.discountPrice != null ? product.discountPrice : product.price}</span>
+        <span className={styles.secondaryPrice} style={{fontSize: fontSizes.secondaryPrice}}>{product.discountPrice != null ? `\$${product.price}` : ""}</span>
         <span className={styles.discount} style={{
                     display: `${(product.discountPrice != null && isDiscountShown) ? "block" : "none"}`,
+                    fontSize: fontSizes.discount,
                 }}>-{Math.floor(100 - (product.discountPrice * 100 / product.price))}%</span>
         </div>
     );
